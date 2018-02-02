5ea895f63f5c5f959840cd687de3b9990490c437
katikireddyh <kvardhan.r@gmail.com>
2018-02-02 08:08:45 -0800

    movie review list

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,54 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
+  before_action :authenticate_user!
+
+  def new
+    @review = Review.new
+  end
+
+  def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.movie_id = @movie.id
+
+    if @review.save
+       redirect_to @movie
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @review.update(review_params)
+        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.json { render :show, status: :ok, location: @review }
+      else
+        format.html { render :edit }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to root_path
+  end
+
+  private
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+end
